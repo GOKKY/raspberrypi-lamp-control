d5be53d2754a5643880e63eeaa167d0ae11ef89f
Max <i.maksim0104@gmail.com>
2025-04-11 00:35:37 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, url_for
+import RPi.GPIO as GPIO
+
+RELAY_PIN = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.output(RELAY_PIN, GPIO.HIGH)
+
+app = Flask(__name__)
+lamp_state = False
+
+@app.route('/')
+def index():
+    return render_template('index.html', lamp_on=lamp_state)
+
+@app.route('/on')
+def turn_on():
+    global lamp_state
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    lamp_state = True
+    return redirect(url_for('index'))
+
+@app.route('/off')
+def turn_off():
+    global lamp_state
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
+    lamp_state = False
+    return redirect(url_for('index'))
+
+@app.route('/toggle')
+def toggle():
+    global lamp_state
+    if lamp_state:
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
+        lamp_state = False
+    else:
+        GPIO.output(RELAY_PIN, GPIO.LOW)
+        lamp_state = True
+    return redirect(url_for('index'))
+
+@app.route('/cleanup')
+def cleanup():
+    GPIO.cleanup()
+    return "GPIO cleaned up"
+
+if __name__ == '__main__':
+    app.run(host='192.168.178.154', port=5000)
